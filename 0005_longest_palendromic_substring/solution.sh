66186a4efa3efbafb7d44ba330000d0ee32766b3
Nick Drian <ndrian@berkeley.edu>
2024-10-03 10:50:38 -0700

    formatting

--- 0005_longest_palendromic_substring/solution.sh
+++ 0005_longest_palendromic_substring/solution.sh
@@ -9,15 +9,12 @@ biggest() {
   size=$3
 
   while [ $ptr1 -ge 0 -a $ptr2 -lt $size -a ${str:$ptr1:1} == ${str:$ptr2:1} ]; do 
-
     ptr1=$(($ptr1 - 1)) 
     ptr2=$(($ptr2 + 1)) 
-
   done
 
   ptr1=$(($ptr1 + 1)) 
   ptr2=$(($ptr2 - 1)) 
-
   len=$(($ptr2 - $ptr1 + 1))
   if [ $len -gt 0 -a ${str:$ptr1:1} == ${str:$ptr2:1} ]; then
     echo ${str:$ptr1:$len}
@@ -32,13 +29,11 @@ res=""
 for (( i=0; i < ${#str} - 1; i++ )); do
 
   temp=$(biggest $i $i ${#str})
-
   if [[ ${#temp} -gt ${#res} ]]; then
     res=$temp
   fi
 
   temp=$(biggest $i $(($i + 1)) ${#str})
-
   if [[ ${#temp} -gt ${#res} ]]; then
     res=$temp
   fi
